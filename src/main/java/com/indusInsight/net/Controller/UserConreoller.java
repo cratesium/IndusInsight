6cc38c6e966d4a208175ca69921ef5caf9c5c3bd
shikhar <cratesium@gmail.com>
2024-10-13 21:50:52 +0530

    putmapping added

--- src/main/java/com/indusInsight/net/Controller/UserConreoller.java
+++ src/main/java/com/indusInsight/net/Controller/UserConreoller.java
@@ -1,6 +1,7 @@
 package com.indusInsight.net.Controller;
 
 import com.indusInsight.net.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,19 +11,20 @@ import java.util.List;
 public class UserConreoller {
     List<User>  list= new ArrayList<>();
 
+
     @GetMapping("/getall")
    public List<User> getAllUsers(){
-        User user = new User(1,"shikhar", "singh" , "shikhar@gmail.com" ,"1234");
-        User user1 = new User(2,"shiva", "sharma" , "shiva@gmail.com" ,"1234");
-        User user2 = new User(3,"shrishti", "varshney" , "shrishti@gmail.com" ,"1234");
-        User user3 = new User(4,"yash", "sharma" , "yash@gmail.com" ,"1234");
-        User user4 = new User(5,"prince", "raj" , "bbc@gmail.com" ,"1234");
+     User  user = new User(1,"shikhar", "singh" , "shikhar@gmail.com" ,"1234");
+//        User user1 = new User(2,"shiva", "sharma" , "shiva@gmail.com" ,"1234");
+//        User user2 = new User(3,"shrishti", "varshney" , "shrishti@gmail.com" ,"1234");
+//        User user3 = new User(4,"yash", "sharma" , "yash@gmail.com" ,"1234");
+//        User user4 = new User(5,"prince", "raj" , "bbc@gmail.com" ,"1234");
 
         list.add(user);
-        list.add(user1);
-        list.add(user2);
-        list.add(user3);
-        list.add(user4);
+//        list.add(user1);
+//        list.add(user2);
+//        list.add(user3);
+//        list.add(user4);
         return  list;
     }
     @GetMapping("getbyid/{id}")
@@ -34,6 +36,28 @@ public class UserConreoller {
 
         }
         return  null;
+    }
+
+     @PutMapping("/update")
+    public  User update(@RequestBody User user){
+
+        if(user.getId()!=null){
+            list.get(0).setId(user.getId());
+        }
+        if (user.getEmail()!=null){
+            list.get(0).setEmail(user.getEmail());
+        }
+        if(user.getFirstName()!=null){
+            list.get(0).setFirstName(user.getFirstName());
+        }
+        if(user.getLastName()!=null){
+            list.get(0).setLastName(user.getLastName());
+        }
+        if (user.getPassword()!=null){
+            list.get(0).setPassword(user.getPassword());
+        }
+
+        return list.get(0);
     }
 
 
